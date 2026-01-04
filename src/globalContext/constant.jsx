814a4f8ab05e6d4cc348856c3d4652c0a70f07fc
jfkfashions <jfkfashions@gmail.com>
2026-01-04 13:09:28 +0100

    Update constant.jsx

--- src/globalContext/constant.jsx
+++ src/globalContext/constant.jsx
@@ -1,3 +1,3 @@
 // Global backend URL (set via Vite environment variable)
 //export const backendUrl = import.meta.env.VITE_BACKEND_URL
-export const backendUrl = process.env.VITE_BACKEND_URL || "https://jfkbe.onrender.com";
+export const backendUrl = process.env.VITE_BACKEND_URL || "https://jfkbe-4zre.onrender.com";
